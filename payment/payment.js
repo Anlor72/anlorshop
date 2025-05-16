c35e9749ae883d30ac50de1349c1a2d0abe99986
tobias61-mg <tobiasmg61@gmail.com>
2025-05-16 16:20:25 -0400

    First version of the Proyect

--- payment/payment.js
+++ payment/payment.js
@@ -1,16 +1,19 @@
-function initializePayPalButton() {
-    paypal.Buttons({
-        createOrder: function(data, actions) {
-            return actions.order.create({
-                purchase_units: [{
-                    amount: { value: '0.20', currency_code: 'USD' }
-                }]
-            });
-        },
-        onApprove: function(data, actions) {
-            return actions.order.capture().then(function(details) {
-                alert('Payment Completed from ' + details.payer.name.given_name);
-            });
-        }
-    }).render('#paypal-button-container');
-}
+fetch('https://anlor-backend.onrender.com/api/paypal-client-id') // ✅ Cambia aquí
+    .then(response => response.json())
+    .then(data => {
+        paypal.Buttons({
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: { value: '27.00', currency_code: 'USD' }
+                    }]
+                });
+            },
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(details) {
+                    alert('Payment Completed from ' + details.payer.name.given_name);
+                });
+            }
+        }).render('#paypal-button-container');
+    })
+    .catch(error => console.error('Error to Load Paypal', error));
